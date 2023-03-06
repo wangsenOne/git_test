72d4e0e7bf92c59940b41207b1b5a54cae41b7c7
wangsen <1968248890@qq.com>
2023-03-06 16:26:01 +0800

    second commit

--- git-test/src/main/java/com/itcast/git/GitTest.java
+++ git-test/src/main/java/com/itcast/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itcast.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("hello git1");
+        System.out.printf("hello git2");
     }
 }
